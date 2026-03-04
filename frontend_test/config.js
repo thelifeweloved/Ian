8b087f42520a0718e6defb8635079b54ec7c0be0
thelifeweloved <thelifeweloved@gmail.com>
2026-03-04 15:39:01 +0900

    config.js 서버 추가

--- frontend_test/config.js
+++ frontend_test/config.js
@@ -2,4 +2,4 @@ const isLocal = window.location.port === "5500" || window.location.hostname === 
 
 const API_BASE_URL = isLocal 
     ? "http://127.0.0.1:8000"  // 내 컴퓨터에서 작업할 때
-    : window.location.origin;
+    : "http://223.130.143.55:8000";  // 서버
